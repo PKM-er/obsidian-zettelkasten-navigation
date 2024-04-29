7eff8fe4d7c376d826ec2ce1092b91fccb5127ee
terrychenzw <114856910+terrychenzw@users.noreply.github.com>
2024-04-29 15:52:27 +0800

    Add files via upload

--- src/suggester/TagSuggester.ts
+++ src/suggester/TagSuggester.ts
@@ -6,7 +6,8 @@ import { TextInputSuggest } from "./suggest";
 export class TagSuggest extends TextInputSuggest<string> {
     
     getSuggestions(inputStr: string): string[] {        
-
+        
+        // @ts-expect-error
         return Object.keys(this.app.metadataCache.getTags());
     }
 
