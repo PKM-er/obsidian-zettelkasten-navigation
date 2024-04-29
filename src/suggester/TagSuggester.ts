8089c08599d27145a19c0d81727e6111eb3ca390
terrychenzw <terrychenzw@gmail.com>
2024-04-29 14:47:04 +0800

    .

---
+++ src/suggester/TagSuggester.ts
@@ -0,0 +1,23 @@
+// Credits go to Liam's Periodic Notes Plugin: https://github.com/liamcain/obsidian-periodic-notes
+
+import { TFile } from "obsidian";
+import { TextInputSuggest } from "./suggest";
+
+export class TagSuggest extends TextInputSuggest<string> {
+    
+    getSuggestions(inputStr: string): string[] {        
+
+        return Object.keys(this.app.metadataCache.getTags());
+    }
+
+
+    renderSuggestion(tag: string, el: HTMLElement): void {
+        el.setText(tag);
+    }
+
+    selectSuggestion(tag: string): void {
+        this.inputEl.value = tag;
+        this.inputEl.trigger("input");
+        this.close();
+    }
+}
